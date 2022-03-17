53824ef205aab209847b8fc9e1c23fdf930db0e8
Mario Daniel Acosta Gonzalez <marito12252@gmail.com>
2022-03-16 22:38:48 -0700

    mysql: cambio hecho por mario colaborador externo

--- app/db/mysql.php
+++ app/db/mysql.php
@@ -1,8 +1,8 @@
 <?php
 class MsqlConnection{
     public $host= '127.0.0.1';
-    public $user= 'root';
-    public $passwd= '';
+    public $user= 'Armando';
+    public $passwd= '123456';
     public $dbname= 'gitdb';
     public $port= 3306;
 
